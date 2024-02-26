4a10509361695d1f9db8580a38a57778f5fbec47
johnnypuskar@yahoo.com <Johnny Puskar>
2024-02-26 02:35:50 -0500

    Token instance testing

--- forever_dm.py
+++ forever_dm.py
@@ -1,8 +1,10 @@
 import json
 import os
 from src.api.api_wrapper import GeminiAPIWrapper
+from src.stats.statblock import Statblock
 from src.map.battlemap import Map, MapProp
 from src.map.instance import Instance
+from src.map.token import Token
 
 test_system = "You are an AI D&D 5e assistant, and will answer any questions about the system as close to the rules as possible."
 
@@ -10,15 +12,15 @@ API_KEY = json.load(open("foreverdm/config/api_config.json", "r"))["GEMINI_API_K
 
 api = GeminiAPIWrapper(API_KEY)
 
-# prompt = "Describe in 1-2 sentences a turn of D&D where a goblin attacks a fighter with a scimitar and deals 7 damage."
-# print("> ",prompt,"\n",api.send_request(prompt))\
-
 map = Map(5,3)
 prop = MapProp("Box", "A wooden box")
-map.add_prop(prop, (2, 2))
+map.add_prop(prop, (4, 2))
 
 instance = Instance(map)
-print(instance.get_context())
-map.remove_prop(prop)
 
-print(instance.get_context())
+token = Token(Statblock("Fighter", "A level 1 fighter"))
+instance.add_token(token, (1, 1))
+
+prompt = f"What is in the room? ROOM: [{instance.get_context()}]"
+
+print("> ",prompt,"\n",api.send_request(prompt))
