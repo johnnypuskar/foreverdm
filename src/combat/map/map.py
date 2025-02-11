61d783ea7740f315f3484868eeacdfd7f736cf51
johnnypuskar@yahoo.com <Johnny Puskar>
2025-02-11 00:33:28 -0500

    Slight optimization when creating tile grid

--- src/combat/map/map.py
+++ src/combat/map/map.py
@@ -6,15 +6,17 @@ class Map:
     def __init__(self, width: int, height: int):
         self._width = width
         self._height = height
-        self._tiles = [[MapTile(x, y) for x in range(width)] for y in range(height)]
+        self._tiles = []
 
         for y in range(self._height):
+            self._tiles.append([])
             for x in range(self._width):
-                tile = self._tiles[y][x]
+                tile = MapTile(x, y)
                 tile.wall_top = self._tiles[y - 1][x].wall_bottom if y > 0 else MapTileWall()
                 tile.wall_left = self._tiles[y][x - 1].wall_right if x > 0 else MapTileWall()
                 tile.wall_bottom = MapTileWall()
                 tile.wall_right = MapTileWall()
+                self._tiles[y].append(tile)
 
     
     @property
