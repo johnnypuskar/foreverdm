1ed6f0dcda0f64432672c7300677f4333e5caa3b
johnnypuskar@yahoo.com <Johnny Puskar>
2025-02-19 00:22:07 -0500

    Added handler function to add or remove conditions

--- src/stats/handlers/effect_handler.py
+++ src/stats/handlers/effect_handler.py
@@ -8,3 +8,11 @@ class EffectHandler:
     def remove_effect(self, effect_name):
         self._statblock._effects.remove(effect_name)
     
+    def add_condition(self, condition_name, duration = -1):
+        condition_effect = self._statblock._effects._condition_manager.new_condition(condition_name, None, duration)
+        self.add_effect(condition_effect, duration)
+    
+    def remove_condition(self, condition_name):
+        if condition_name not in self._statblock._effects._condition_manager.CONDITIONS.keys():
+            raise ValueError(f"{condition_name} is not a valid condition.")
+        self.remove_effect(condition_name)
