c33108d03a7081ed201c3e930e73e06ac0019adf
johnnypuskar@yahoo.com <Johnny Puskar>
2025-02-27 20:21:32 -0500

    Removed cap from modify speed preventing speed from being increased by multiply and set modifiers

--- src/stats/handlers/movement_handler.py
+++ src/stats/handlers/movement_handler.py
@@ -10,33 +10,33 @@ class MovementHandler:
         if "walk" in speed_modifier.keys():
             walk_cost = speed_modifier["walk"]["value"]
             if speed_modifier["walk"]["operation"] == "set":
-                walk_cost = max(0, self._statblock._speed.walk - walk_cost)
+                walk_cost = self._statblock._speed.walk - walk_cost
             elif speed_modifier["walk"]["operation"] == "multiply":
-                walk_cost = max(0, self._statblock._speed.walk * (1 - walk_cost))
+                walk_cost = self._statblock._speed.walk * (1 - walk_cost)
         if "fly" in speed_modifier.keys():
             fly_cost = speed_modifier["fly"]["value"]
             if speed_modifier["fly"]["operation"] == "set":
-                fly_cost = max(0, self._statblock._speed.fly - fly_cost)
+                fly_cost = self._statblock._speed.fly - fly_cost
             elif speed_modifier["fly"]["operation"] == "multiply":
-                fly_cost = max(0, self._statblock._speed.fly * (1 - fly_cost))
+                fly_cost = self._statblock._speed.fly * (1 - fly_cost)
         if "swim" in speed_modifier.keys():
             swim_cost = speed_modifier["swim"]["value"]
             if speed_modifier["swim"]["operation"] == "set":
-                swim_cost = max(0, self._statblock._speed.swim - swim_cost)
+                swim_cost = self._statblock._speed.swim - swim_cost
             elif speed_modifier["swim"]["operation"] == "multiply":
-                swim_cost = max(0, self._statblock._speed.swim * (1 - swim_cost))
+                swim_cost = self._statblock._speed.swim * (1 - swim_cost)
         if "climb" in speed_modifier.keys():
             climb_cost = speed_modifier["climb"]["value"]
             if speed_modifier["climb"]["operation"] == "set":
-                climb_cost = max(0, self._statblock._speed.climb - climb_cost)
+                climb_cost = self._statblock._speed.climb - climb_cost
             elif speed_modifier["climb"]["operation"] == "multiply":
-                climb_cost = max(0, self._statblock._speed.climb * (1 - climb_cost))
+                climb_cost = self._statblock._speed.climb * (1 - climb_cost)
         if "burrow" in speed_modifier.keys():
             burrow_cost = speed_modifier["burrow"]["value"]
             if speed_modifier["burrow"]["operation"] == "set":
-                burrow_cost = max(0, self._statblock._speed.burrow - burrow_cost)
+                burrow_cost = self._statblock._speed.burrow - burrow_cost
             elif speed_modifier["burrow"]["operation"] == "multiply":
-                burrow_cost = max(0, self._statblock._speed.burrow * (1 - burrow_cost))
+                burrow_cost = self._statblock._speed.burrow * (1 - burrow_cost)
         
         cost = MovementCost(walk_cost, fly_cost, swim_cost, climb_cost, burrow_cost)
         return self._statblock._speed.move(cost)
