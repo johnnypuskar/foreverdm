bb477f1372c713b62076109d9d2e3c7fbfb0c429
johnnypuskar@yahoo.com <Johnny Puskar>
2025-02-27 20:33:04 -0500

    Added function to allow user roller to just prompt the terminal for the roll result

--- src/util/dice/user_roller.py
+++ src/util/dice/user_roller.py
@@ -4,3 +4,12 @@ class UserRoller(DiceRoller):
     def __init__(self, user_id):
         super().__init__()
         self._user_id = user_id
+    
+    def roll_d20(self, advantage = False, disadvantage = False):
+        result = None
+        while result is None or not isinstance(result, int):
+            try:
+                result = int(input("Enter d20 result: "))
+            except ValueError:
+                print("Invalid input. Please enter a valid number.")
+        return result
