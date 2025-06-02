07205c7f8ecbcebf2e6e001e081c07314085f632
johnnypuskar@yahoo.com <Johnny Puskar>
2025-06-02 04:52:37 -0400

    reworked functions in statblocks table script

--- server/backend/database/statblocks.py
+++ server/backend/database/statblocks.py
@@ -4,44 +4,28 @@ from server.backend.database.table import Table
 class StatblocksTable(Table):
     
     @staticmethod
-    def get_statblock_data(statblock_id, campaign_id):
-        with Table.cursor() as cur:
-            cur.execute("SELECT data FROM statblocks WHERE id = %s AND campaign_id = %s", (statblock_id, campaign_id))
-            result = cur.fetchone()
-            if result is None:
-                raise Errors.StatblockNotFound()
-            return result[0]
-        raise Errors.DataAccessError()
+    def validate_location_from_credentials(session_id, campaign_id, statblock_id):
+        if session_id is None:
+            raise Errors.SessionNotFound()
+        
+        if campaign_id is None:
+            raise Errors.UserNotInCampaign()
 
-    @staticmethod
-    def get_statblock_location_id(statblock_id, campaign_id):
         with Table.cursor() as cur:
-            cur.execute("SELECT location_id FROM statblocks WHERE id = %s AND campaign_id = %s", (statblock_id, campaign_id))
-            result = cur.fetchone()
-            if result is None:
-                raise Errors.StatblockNotFound()
-            return result[0]
-        raise Errors.DataAccessError()
+            cur.execute("SELECT user_id FROM sessions WHERE session_key = %s", (session_id,))
+            user_id = cur.fetchone()
+            if user_id is None:
+                raise Errors.SessionNotFound()
+            user_id = user_id[0]
 
-    @staticmethod
-    def get_statblocks_at_location_id(location_id, campaign_id):
-        with Table.cursor() as cur:
-            cur.execute("SELECT id FROM statblocks WHERE location_id = %s AND campaign_id = %s", (location_id, campaign_id))
-            results = cur.fetchall()
-            return [result[0] for result in results] if results else []
-        raise Errors.DataAccessError()
-
-    @staticmethod
-    def is_user_in_campaign(user_id, campaign_id):
-        with Table.cursor() as cur:
             cur.execute("SELECT COUNT(*) FROM campaign_users WHERE user_id = %s AND campaign_id = %s", (user_id, campaign_id))
-            result = cur.fetchone()
-            return result[0] > 0 if result else False
-        raise Errors.DataAccessError()
+            campaign_users = cur.fetchone()
+            if campaign_users is None or campaign_users[0] == 0:
+                raise Errors.UserNotInCampaign()
             
-    @staticmethod
-    def validate_user_statblock_ownership(user_id, statblock_id, campaign_id):
-        with Table.cursor() as cur:
+            if statblock_id is None:
+                raise Errors.StatblockNotFound()
+            
             cur.execute("""
                 SELECT location_id FROM statblocks WHERE
                 user_id = %s AND campaign_id = %s AND id = %s
@@ -58,10 +42,60 @@ class StatblocksTable(Table):
             # Assume via process of elimination
             raise Errors.UserNotOwnStatblock()
         raise Errors.DataAccessError()
-    
+
     @staticmethod
-    def get_user_statblocks(user_id, campaign_id):
+    def validate_user_statblocks_in_campaign(session_id, campaign_id):
+        if session_id is None:
+            raise Errors.SessionNotFound()
+        
+        if campaign_id is None:
+            raise Errors.UserNotInCampaign()
+
         with Table.cursor() as cur:
-            cur.execute("SELECT id, name FROM statblocks WHERE user_id = %s AND campaign_id = %", (user_id, campaign_id))
+            cur.execute("SELECT user_id FROM sessions WHERE session_key = %s", (session_id,))
+            user_id = cur.fetchone()
+            if user_id is None:
+                raise Errors.SessionNotFound()
+            user_id = user_id[0]
+
+            cur.execute("SELECT id, name FROM statblocks WHERE user_id = %s AND campaign_id = %s", (user_id, campaign_id))
             result = cur.fetchall()
             return [(statblock_id, name) for statblock_id, name in result] if result else []
+        raise Errors.DataAccessError()
+    
+    @staticmethod
+    def get_users_statblocks(user_id, campaign_id):
+        with Table.cursor() as cur:
+            cur.execute("SELECT id FROM statblocks WHERE user_id = %s AND campaign_id = %s", (user_id, campaign_id))
+            result = cur.fetchall()
+            return [statblock_id for statblock_id, in result] if result else []
+        raise Errors.DataAccessError()
+
+    @staticmethod
+    def get_location(statblock_id, campaign_id):
+        with Table.cursor() as cur:
+            cur.execute("SELECT location_id FROM statblocks WHERE id = %s AND campaign_id = %s", (statblock_id, campaign_id))
+            result = cur.fetchone()
+            if result is not None:
+                return result[0]
+            raise Errors.StatblockNotFound()
+        raise Errors.DataAccessError()
+
+    @staticmethod
+    def update_location(statblock_id, campaign_id, location_id):
+        with Table.cursor() as cur:
+            cur.execute("UPDATE statblocks SET location_id = %s WHERE id = %s AND campaign_id = %s", (location_id, statblock_id, campaign_id))
+            if cur.rowcount == 0:
+                raise Errors.StatblockNotFound()
+            return
+        raise Errors.DataAccessError()
+
+    @staticmethod
+    def insert_statblock(statblock_id, name, campaign_id, user_id, location_id, data):
+        with Table.cursor() as cur:
+            cur.execute("""
+                INSERT INTO statblocks (id, name, campaign_id, user_id, location_id, data)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """, (statblock_id, name, campaign_id, user_id, location_id, data))
+            return
+        raise Errors.DataAccessError()
