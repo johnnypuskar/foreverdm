c64c5c1a06b0484f2dcb6c93d607279267ee92f7
johnnypuskar@yahoo.com <Johnny Puskar>
2025-06-03 00:27:45 -0400

    spacing

--- server/backend/instances/acts/world_act.py
+++ server/backend/instances/acts/world_act.py
@@ -7,7 +7,6 @@ class WorldAct(Act):
     def __init__(self, campaign_id, location_id, statblock_ids = set()):
         super().__init__(campaign_id, location_id, statblock_ids)
         self.type = ActType.WORLD
-
     
     def get_view_data(self, statblock_id):
         data = super().get_view_data(statblock_id)
