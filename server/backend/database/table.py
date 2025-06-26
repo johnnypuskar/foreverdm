8d90702a5c47cd7f0ff07e3a119ecd53facc3a43
johnnypuskar@yahoo.com <Johnny Puskar>
2025-06-26 16:07:23 -0400

    reworked table class to allow for functions using a cursor with multiple function calls

--- server/backend/database/table.py
+++ server/backend/database/table.py
@@ -5,16 +5,34 @@ from contextlib import contextmanager
 class Table:
     DB_NAME = "foreverdm"
 
+    def __init__(self):
+        self._conn = Table._new_connection()
+        self._cursor = self._conn.cursor()
+        print("INSTANCE CURSOR", self._cursor)
+    
+    def commit(self, close = True):
+        self._conn.commit()
+        if close:
+            self.close()
+    
+    def close(self):
+        self._conn.close()
+
     @staticmethod
-    @contextmanager
-    def cursor():
-        conn = psycopg.connect(f"""\
+    def _new_connection():
+        return psycopg.connect(f"""\
             dbname={Table.DB_NAME} \
             user={os.getenv('POSTGRES_USER')} \
             password={os.getenv('POSTGRES_PASSWORD')} \
             host={os.getenv('DB_ADDRESS')} \
             port={os.getenv('DB_PORT')} \
         """)
+
+    @staticmethod
+    @contextmanager
+    def cursor():
+        print("CALLED THE STATIC CURSOR METHOD")
+        conn = Table._new_connection()
         try:
             yield conn.cursor()
         finally:
